44323a2fa9a25c1e337eb4843f61e85af74e6a07
lgoudin <lgoudin@inflexsys.com>
2021-12-07 19:51:19 +0100

    Fixed errors

--- src/features/word/ui/cubits/rhymes-cubit.ts
+++ src/features/word/ui/cubits/rhymes-cubit.ts
@@ -1,19 +1,13 @@
-import axios from "axios";
 import { Cubit } from "bloc-react";
-import { WordRemoteDatasourceImpl } from "../../data/datasources/remote-datasource";
-import WordRepositoryImpl from "../../data/repositories/repository-impl";
 import WordRepository from "../../domain/repositories/repository";
 import { RhymesError, RhymesInitial, RhymesLoaded, RhymesLoading, RhymesState } from "./rhymes-state";
 
 export default class RhymesCubit extends Cubit<RhymesState> {
   repository: WordRepository;
 
-  constructor() {
+  constructor(repository: WordRepository) {
     super(new RhymesInitial());
-    const axiosInstance = axios.create({ baseURL: 'https://api.datamuse.com/' });
-    const wordRemoteDatasource = new WordRemoteDatasourceImpl(axiosInstance);
-    const wordRepository = new WordRepositoryImpl(wordRemoteDatasource);
-    this.repository = wordRepository
+    this.repository = repository;
   }
 
   async getRhymes(word: string) {
